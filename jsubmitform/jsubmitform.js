66444d5454f2bf8ca98af6940f41772b2e7310a4
ajikcey <ajikcey@mail.ru>
2014-06-01 18:43:14 +0000

    Update jsubmitform.js

--- jsubmitform/jsubmitform.js
+++ jsubmitform/jsubmitform.js
@@ -2,9 +2,10 @@
 
 Parameters:
 
-img             - image loading <img src="...">
-debug           - show debug info
-error           - show error info
+debug           - show debug info (default = false)
+error           - show error info (default = true)
+animate         - show loading animate (default = true)
+handle          - handle request (default = false)
 
 Functions:
 
@@ -15,9 +16,9 @@ after({form: form});
 
 Elements:
 
-#jsf_error      - element for response
-#jsf_loading    - element for loading image
+#jsf_error      - element for response/error
 #jsf_submit     - button submit
+#jsf_animate    - element for loading animate
 
 Attributes:
 
@@ -25,6 +26,119 @@ jsf_necessary   - required fields and value
 
 */
 function jsubmitform(form, params) {
+
+    // validation
+    if (!form || typeof(form) != "object") {
+        return false;
+    }
+
+    // default
+    if (!params || typeof(params) != "object") {
+        params = {};
+    }
+    
+    if (!params.debug) {
+        params.debug = false;
+    }
+    if (!params.error) {
+        params.error = true;
+    }
+    if (!params.animate) {
+        params.animate = true;
+    }
+    if (!params.handle) {
+        params.handle = false;
+    }
+
+    form.find('#jsf_submit').on('click', function () {
+        
+        hideel(form.find('#jsf_error'));
+        var err_b = false;
+        
+        // before
+        callf(params.before, {form: form});
+        
+        form.find('*[jsf_necessary]').each(function() {
+            
+            // if not filled in the required fields
+            if (!$(this).val()) {
+                
+                // error
+                if (params.error) {
+                    showel(form.find('#jsf_error'), $(this).attr("jsf_necessary"));
+                }
+                err_b = true;
+                
+                return false;
+            } else {
+                
+            }
+        });
+
+        if (!err_b) {
+            sub = true;
+            
+            // desabled button
+            form.find('#jsf_submit').attr('disabled', 'disabled');
+            
+            // animate
+            if (params.animate) {
+                showel(form.find('#jsf_animate'), '<div id="facebookG"><div id="blockG_1" class="facebook_blockG"></div><div id="blockG_2" class="facebook_blockG"></div><div id="blockG_3" class="facebook_blockG"></div></div>');
+            }
+
+            // ajax
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize(),
+                success: function (data) {
+                    // success
+                    callf(params.success, {form: form, data: data});
+                    
+                    // handle
+                    if (params.handle) {
+                        callf('handle_request', {form: form, data: data});
+                    }
+                },
+                error: function() {
+                    // debug
+                    if (params.debug) {
+                        showel(form.find('#jsf_error'), 'Debug: no response');
+                    }
+                    // noresponse
+                    callf(params.noresponse, {form: form});
+                }
+            })
+            .always(function() {
+                // after
+                callf(params.after, {form: form});
+                
+                // enbled button
+                form.find('#jsf_submit').removeAttr("disabled");
+                
+                // hide animate
+                hideel(form.find('#jsf_animate'));
+            });
+        }
+        return false;
+    });
+    
+    // handle request
+    function handle_request(obj) {
+        try {
+            var o_data = $.parseJSON(obj.data);
+            if (o_data.success) {
+                showel(obj.form.find('#JSf_error'), o_data.success);                    
+             } else {
+                
+             }
+        } catch (e) {
+            // debug
+            if (params.debug) {
+                showel(obj.form.find('#jsf_error'), 'Debug: incorrect data');
+            }
+        }
+    }
     
     // call function "f" this parameters "p" as object
     function callf(f, p) {
@@ -42,111 +156,5 @@ function jsubmitform(form, params) {
     function hideel(where) {
         where.html("").hide();
     }
-    
-    // validation
-    if (!form || typeof(form) != "object") {
-        return false;
-    }
 
-    // default
-    if (!params || typeof(params) != "object") {
-        params = {};
-    }
-    
-    if (!params.img) {
-        params.img = "/jsubmitform/load.gif";
-    }
-    
-    if (!params.error) {
-        params.error = true;
-    }
-
-    var sub = false;    // send request flag
-
-    form.find('#jsf_submit').on('click', function () {
-        
-        hideel(form.find('#jsf_error'));
-        hideel(form.find('#jsf_loading'));
-
-        var err_b = false;
-        
-        // before
-        callf(params.before, {form: form});
-        
-        form.find('*[jsf_necessary]').each(function() {
-            
-            // if not filled in the required fields
-            if (!$(this).val()) {
-                
-                // error
-                if (params.error) {
-                    showel(form.find('#jsf_error'), $(this).attr("jsf_necessary"));
-                }
-
-                err_b = true;
-                
-                return false;
-            } else {
-                
-            }
-        });
-
-        if (!sub && !err_b) {
-            sub = true;
-            
-            // image loading insert to the place
-            if (params.img) {
-                showel(form.find('#jsf_loading'), '<img src="' + params.img + '" /> ');
-            }
-            
-            // ajax
-            $.ajax({
-                type: form.attr('method'),
-                url: form.attr('action'),
-                data: form.serialize(),
-                success: function (data) {
-
-                    //try {
-                        //var o = $.parseJSON(data);
-                        //if (o.success) {
-                        //    
-                        //    showel(form.find('#JSf_error'), o.success);
-                        //
-                        //    // success
-                            callf(params.success, {form: form, data: data});
-                        //} else {
-                            // error
-                        //    callf(params.error, {
-                        //        data: data,
-                        //        form: form
-                        //    });
-                        //}
-                    //} catch (e) {
-                        
-                        
-                        // warning
-                        //callf(params.error, {form: form});
-                    //}
-                },
-                error: function() {
-                    
-                    // debug
-                    if (params.debug) {
-                        showel(form.find('#jsf_error'), 'Debug: no response');
-                    }
-                        
-                    // noresponse
-                    callf(params.noresponse, {form: form});
-                }
-            })
-            .always(function() {
-                // after
-                callf(params.after, {form: form});
-                
-                hideel(form.find('#jsf_loading'));
-                sub = false;
-            });
-        }
-        return false;
-    });
 }
